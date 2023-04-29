1bcd17618f28aa94e179098f4cf3a47033ccf709
Claudio <claudio@syrianarchive.org>
2023-04-29 13:31:24 +0100

    producing a small utility to easily execute the sequence of commands

---
+++ utils/PT.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+scripts/country-processor.mjs --source rawlists/PT-governement-1.txt --name PTg
+scripts/country-processor.mjs --source rawlists/PT-governement-2.txt --name PTg
+scripts/country-processor.mjs --source rawlists/PT-governement-3.txt --name PTg
